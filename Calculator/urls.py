88d70ca69c73ada9b7798bd819c2a88a0b0d08fa
AdityaRajThakur <adityaraj715426@gmail.com>
2021-04-26 23:25:39 -0700

    fourth commit

--- Calculator/urls.py
+++ Calculator/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.hello),
+    path('home/',views.hello),
 ]
--- Calculator/views.py
+++ Calculator/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def hello(request):
     # return HttpResponse("Hello World")
-    return render(request,'index.html')
+    return render(request,'result.html')
